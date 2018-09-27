248ae55859f69e0517394593eb474c9d76326f02
Jeija <norrepli@gmail.com>
2018-09-27 15:37:50 +0200

    Improve downlink decoding script readability

--- downlink/demodulate.py
+++ downlink/demodulate.py
@@ -1,26 +1,46 @@
 #!/usr/bin/env python3
+# renard-phy downlink demodulation script
 
-import matplotlib.pyplot as plt
 import scipy, scipy.signal, scipy.io, scipy.io.wavfile
 import numpy as np
+import argparse
 import sys
+
+###########################
+#    Parse CLI argumets   #
+###########################
+parser = argparse.ArgumentParser()
+parser.add_argument("wavfile", help="WAV file containing the downlink recording to be demodulated")
+parser.add_argument("-p", "--plot", action="store_true", default = False, help = "Use matplotlib to display the baseband signal and where in that baseband signal a preamble was detected")
+parser.add_argument("-d", "--decode", action="store_true", default = False, help = "Use renard to brute-force-decode payload content of downlink frame")
+args = parser.parse_args()
 
 ###########################
 #       Definitions       #
 ###########################
-FILENAME = sys.argv[1]
-
-# General definitions
+# Baudrate of the downlink signal
 DOWNLINK_BAUDRATE = 600
+
+# Minimum frequency resolution of FFT frequency estimation
 MIN_FREQ_RES = 10
-LPF_FREQ = 4000
+
+# Cutoff frequency for low pass filter applied after mixing
+LPF_FREQ = 3500
+
+# Symbols that the downlink preamble consists of (antipodally mapped 0x2aaaaaaaaaaaaaaaaaaaaab227)
 DOWNLINK_PREAMBLE = [-1] + [-1, 1] * 45 + [1, -1, -1, 1, -1, -1, -1, 1, -1, -1, 1, 1, 1]
 
-# Demodulated signal is upsampled to this sample rate to allow for better preamble detection
+# Demodulated signal is upsampled to this sample rate to allow for more accurate preamble detection
 DEMOD_SAMPRATE = 300000
 
+# Length of Sigfox downlink frame in bits (excluding preamble)
+FRAMELENGTH_WITHOUT_PREAMBLE = 120
+
+# Total length of Sigfox downlink frame, including preamble
+FRAMELENGTH = FRAMELENGTH_WITHOUT_PREAMBLE + len(DOWNLINK_PREAMBLE)
+
 ###########################
-#    Utility functions    #
+#    Utility Functions    #
 ###########################
 def nextpow2(limit):
 	n = 1
@@ -31,7 +51,7 @@ def nextpow2(limit):
 ###########################
 #     Recording Input     #
 ###########################
-[Fs, IQ] = scipy.io.wavfile.read(FILENAME)
+[Fs, IQ] = scipy.io.wavfile.read(args.wavfile)
 
 # IQ offset / imbalance correction
 I_nooffset = IQ[:, 0] - sum(IQ[:, 0]) / len(IQ[:, 0])
@@ -45,7 +65,6 @@ signal /= np.mean(abs(signal))
 ############################
 #   Frequency Estimation   #
 ############################
-# Get very approximate frequency estimation
 nfft = nextpow2(Fs / MIN_FREQ_RES)
 print("Estimating frequency using " + str(nfft) + "-length FFT")
 f, spec = scipy.signal.welch(signal, Fs, noverlap = 0, nperseg = nfft / 4, nfft = nfft, return_onesided = False)
@@ -54,24 +73,24 @@ f, spec = scipy.signal.welch(signal, Fs, noverlap = 0, nperseg = nfft / 4, nfft 
 sigFreq = sum(f * abs(spec)) / sum(abs(spec))
 
 ############################
-#      Mix to baseband     #
+#      Mix to Baseband     #
 ############################
 mixer = np.exp(-1.0j * 2.0 * np.pi * sigFreq / Fs * np.arange(0, len(signal)))
 baseband = signal * mixer
 lpf_b, lpf_a = scipy.signal.butter(4, LPF_FREQ / Fs)
 baseband = scipy.signal.lfilter(lpf_b, lpf_a, baseband)
 
+# arctan demodulator: get FM baseband singnal
 phase = np.unwrap(np.angle(baseband))
 freq = np.diff(phase)
 demod = freq - np.mean(freq)
 
-# Upsample signal - simply repeat samples instead of using scipy.signal.resample (much faster)
-print("Upsampling demodulated signal...")
+# Upsample baseband signal: Simply repeat samples instead of using scipy.signal.resample (much faster)
 #demod = scipy.signal.resample(demod, int(len(demod) * DEMOD_SAMPRATE / Fs))
 demod = [demod[int(i * Fs / DEMOD_SAMPRATE)] for i in range(int(len(demod) * DEMOD_SAMPRATE / Fs))]
 
 ############################
-#       Find preamble      #
+#       Find Preamble      #
 ############################
 print("Finding preamble...")
 preamble = [s for s in DOWNLINK_PREAMBLE for _ in range(int(DEMOD_SAMPRATE / DOWNLINK_BAUDRATE))]
@@ -84,23 +103,24 @@ preamble_offset = np.argmax(xcorr)
 # Estimate DC offset by calculating mean value of signal during preamble
 demod = demod - np.mean(demod[preamble_offset:preamble_offset + len(preamble)])
 
-plt.plot(demod)
-plt.plot([0] * preamble_offset + preamble)# + ([1] * int(DEMOD_SAMPRATE / DOWNLINK_BAUDRATE) + [-1] * int(DEMOD_SAMPRATE / DOWNLINK_BAUDRATE)) * 60)
-plt.show()
+############################
+#      Optional: Plot      #
+############################
+if args.plot:
+	import matplotlib.pyplot as plt
+	plt.plot(demod)
+	plt.plot([0] * preamble_offset + preamble)
+	plt.show()
 
 ############################
-#       Turn into bits     #
+#       Turn into Bits     #
 ############################
 samples_per_symbol = DEMOD_SAMPRATE / DOWNLINK_BAUDRATE
+first_sample_offset = int(preamble_offset + samples_per_symbol / 2)
 
-bits = []
-for i in (int(preamble_offset + samples_per_symbol / 2) + np.arange(0, samples_per_symbol * (len(DOWNLINK_PREAMBLE) + 120), samples_per_symbol)):
-	if demod[int(i)] < 0:
-		bits = bits + [0]
-	else:
-		bits = bits + [1]
+bits = ""
+for i in (np.arange(0, FRAMELENGTH) * samples_per_symbol + first_sample_offset):
+	bits = bits + ("0" if demod[int(i)] < 0 else "1")
 
-bitstring = "".join([str(b) for b in bits])
-print(bitstring)
-hexstring = str(hex(int(bitstring, 2)))
-print("Payload: " + hexstring[int(2 + len(DOWNLINK_PREAMBLE) / 4):])
+hexstring = str(hex(int(bits, 2)))
+print("Frame: " + hexstring[int(2 + len(DOWNLINK_PREAMBLE) / 4):])
