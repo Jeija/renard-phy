8a33c26e88c277e42e6e61f9ab37a86bdb9b998e
Jeija <norrepli@gmail.com>
2018-10-20 10:02:34 +0200

    Add scripts for uplink / downlink modulation and renard submodule

---
+++ downlink/modulate_hackrf.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python3
+# Usage: downlink_hackrf_transmit.py <UPLINK_FREQUENCY> <renard arguments>
+# * First argument is frequency of first corresponding uplink frame (in Hz).
+#   This frequency is used to calculate the downlink frequency by adding a constant of 1.395 MHz.
+# * All additional arguments are simply passed on to renard.
+
+import matplotlib.pyplot as plt
+import scipy.signal
+import numpy as np
+import subprocess
+import binascii
+import struct
+import sys
+import os
+
+SAMPLERATE = 2000000
+BAUDRATE = 600
+
+GFSK_BANDWIDTH = 1600
+SILENCE_DURATION = 0.2
+
+GAUSSFILT_SIZE = 1.2
+GAUSSFILT_SIGMA = 0.00015 * SAMPLERATE
+
+OUTFILE = "/tmp/hackrf_transmission.raw"
+
+HRFTRANS_CMD = "/usr/bin/hackrf_transfer"
+UPLINK_FREQ = int(sys.argv[1])
+HRF_CARRIER_FREQ = UPLINK_FREQ + 1395000
+
+# Use renard to obtain a hexadecimal representation of the sigfox frames to send
+# Always run renard in frame-building mode (-b)
+RENARD_BIN = os.path.join(os.path.dirname(os.path.realpath(__file__)), os.pardir, "renard", "build", "renard")
+RENARD_CMD = [RENARD_BIN] + ["dlencode"] + sys.argv[2:]
+
+try:
+	FRAME_HEX = subprocess.check_output(RENARD_CMD, stderr = subprocess.STDOUT)
+except subprocess.CalledProcessError as e:
+	print(RENARD_BIN + " subprocess failed. Error:")
+	print(e.output.decode("UTF-8"))
+	sys.exit(0)
+
+hexstring = FRAME_HEX.decode("UTF-8").rstrip()
+print("Calculating signal for frame: " + hexstring)
+
+# convert hexadecimal representation to binary string
+binstring = "".join(format(c, "08b") for c in binascii.unhexlify(hexstring))
+
+# create unfiltered baseband signal including ramp-up / ramp-down
+# (one value in 'baseband_symbols' represents one symbol)
+baseband_symbols = []
+baseband_symbols = baseband_symbols + [0, 0]
+for bit in binstring:
+	baseband_symbols = baseband_symbols + [1 if bit == '1' else -1]
+baseband_symbols = baseband_symbols + [0, 0]
+
+# create baseband frequency-over-sample signal
+# offset signal by a frequency of -GFSK_BANDWIDTH, so that HackRF One's DC component doesn't disturb signal
+# receiver seems to tolerate negative offsets better than positive offsets
+baseband_freq = [((s - 2) * GFSK_BANDWIDTH / 2) for s in baseband_symbols for _ in range(int(SAMPLERATE / BAUDRATE))]
+
+# filter baseband frequency-over-sample signal with gaussian pulse
+gaussfilt = scipy.signal.gaussian(int(SAMPLERATE / BAUDRATE * GAUSSFILT_SIZE), GAUSSFILT_SIGMA, False)
+gaussfilt = gaussfilt / sum(gaussfilt)
+baseband_freq = scipy.signal.convolve(baseband_freq, gaussfilt)
+baseband_freq = baseband_freq[int(len(gaussfilt) / 2):-int(len(gaussfilt) / 2)]
+
+# create "complex" baseband signal
+# 'baseband' is actually a signal consisting of 8-bit signed integer
+# I / Q values, alternating between I and Q
+baseband = []
+phase = 1
+for instFreq in baseband_freq:
+	if (len(baseband) % 5000 == 0):
+		print(str(len(baseband)) + " / " + str(len(baseband_freq) * 2))
+	phase = phase / np.abs(phase)
+	phase = phase * np.exp(1.0j * 2 * np.pi * instFreq / SAMPLERATE)
+	baseband.append(np.int8(np.real(phase) * 127))
+	baseband.append(np.int8(np.imag(phase) * 127))
+
+# Add a bit of "silence" after transmission
+baseband = baseband  + [np.int8(0)] * int(SILENCE_DURATION * SAMPLERATE * 2)
+
+print("Writing output...")
+with open(OUTFILE, "wb") as output:
+	output.write(struct.pack("%sb" % len(baseband), *baseband))
+
+hftrans = [HRFTRANS_CMD] + ["-t", OUTFILE, "-s", str(SAMPLERATE), "-f", str(HRF_CARRIER_FREQ), "-x", "40"]
+print(" ".join(hftrans))
+subprocess.call(hftrans, stderr = subprocess.STDOUT)
---
+++ uplink/modulate_hackrf.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python3
+# Generate uplink frame with renard and transmit uplink with HackRF One.
+# Usage: modulate_hackrf.py [RENARD CLI ARGS]
+# All command line arguments to modulate_hackrf.py will just be forwarded to "renard ulencode".
+
+import matplotlib.pyplot as plt
+import scipy.signal
+import numpy as np
+import subprocess
+import binascii
+import struct
+import sys
+import os
+
+SAMPLERATE = 2000000
+BAUDRATE = 100
+
+GAUSSFILT_SIZE = 0.9
+GAUSSFILT_SIGMA = 0.0014 * SAMPLERATE
+
+IF_CARRIER_FREQ = 4000
+
+PAUSE_DURATION = 0.5
+
+OUTFILE = "/tmp/hackrf_transmission.raw"
+
+HRFTRANS_CMD = "/usr/bin/hackrf_transfer"
+HRF_CARRIER_FREQ = "868125700"
+
+# Use renard to obtain a hexadecimal representation of the sigfox frames to send
+# Always run renard in frame-building mode (-b)
+RENARD_BIN = os.path.join(os.path.dirname(os.path.realpath(__file__)), os.pardir, "renard", "build", "renard")
+RENARD_CMD = [RENARD_BIN] + ["ulencode"] + sys.argv[1:]
+
+try:
+	FRAMES_HEX = subprocess.check_output(RENARD_CMD, stderr = subprocess.STDOUT)
+except subprocess.CalledProcessError as e:
+	print(RENARD_BIN + " subprocess failed. Error:")
+	print(e.output.decode("UTF-8"))
+	sys.exit(0)
+
+FRAMES_HEX = FRAMES_HEX.decode("UTF-8").split("\n")
+
+# total signal / baseband are signals where each value represents one symbol value
+total_signal = []
+
+for hexstring in FRAMES_HEX:
+	if (len(hexstring) == 0):
+		continue
+
+	print("Adding frame: " + hexstring)
+
+	# convert hexadecimal representation to binary string
+	binstring = "".join(format(c, "08b") for c in binascii.unhexlify(hexstring))
+
+	# add "1"s at beginning and end of bitstring for ramp-up / ramp-down
+	binstring = "1" + binstring + "11"
+
+	# create unfiltered baseband signal
+	baseband = []
+	phase = True
+	for bit in binstring:
+		baseband = baseband + [1 if phase else -1]
+		phase = phase if bit == "1" else not phase
+
+	# mix baseband signal with intermediate carrier frequency
+	total_signal.extend(baseband)
+
+	# add pause after the transmission
+	# the pause duration seems to be irrelevant
+	total_signal.extend([0] * int(PAUSE_DURATION * BAUDRATE))
+
+# write complete signal to hackrf RAW I/Q file:
+# * Repeat bits int(SAMPLERATE / BAUDRATE) times to achieve sample rate
+# * Hack: Set I and Q to always be the same value by multiplying by "2"
+print("Quantizing...")
+iqsignal = np.int8(np.real(total_signal) * 127)
+rawsignal = [s for s in iqsignal for _ in range(2 * int(SAMPLERATE / BAUDRATE))]
+
+print("Writing output...")
+with open(OUTFILE, "wb") as output:
+	output.write(struct.pack("%sb" % len(rawsignal), *rawsignal))
+
+hftrans = [HRFTRANS_CMD] + ["-t", OUTFILE, "-s", str(SAMPLERATE), "-f", str(HRF_CARRIER_FREQ), "-x", "47", "-a", "1"]
+print(" ".join(hftrans))
+subprocess.call(hftrans, stderr = subprocess.STDOUT)
